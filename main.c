df54844592c20bb5537aadf9dafca53d1f7085d8
hivashah <76765644+hivashah@users.noreply.github.com>
2022-12-28 12:08:46 +0330

    first word method

--- main.c
+++ main.c
@@ -1,18 +1,35 @@
+#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+#define MAXLIST 100 // max number of commands to be suppo
+void firstWord(char *string){
+    char *found;
 
-void printDir()
-{
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    printf("\nDir: %s", cwd);
+    found= strsep(&string," ");
+    printf("%s\n",found);
 }
 
-int main()
-{
-    while (1) {
-        // print shell line
-        printDir();
-        // take input
-
-    }
+// Function to take input
+int takeInput(char* str) {
+    scanf("%s", str);
     return 0;
+    }
+
+
+int main() {
+    char inputString[100];
+    char string[100];
+    takeInput(string);
+    firstWord(string);
+
+    return 0;
+
 }
+
+
