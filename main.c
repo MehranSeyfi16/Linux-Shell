60f4d9d901adfc8f18f95d10724fa7c9562a854b
hivashah <76765644+hivashah@users.noreply.github.com>
2022-12-29 01:43:12 +0330

    commnad handler method added

--- main.c
+++ main.c
@@ -7,102 +7,101 @@
 #include<sys/wait.h>
 #include<readline/readline.h>
 #include<readline/history.h>
-#define MAXLIST 100 // max number of commands to be suppo
+
 void firstWord(char *string){
     char *found;
-
     found= strsep(&string," ");
     printf("%s\n",found);
 }
 
-// Function to take input
 int takeInput(char* str) {
     scanf("%s", str);
     return 0;
 }
 
-/* Signal Handler for SIGINT */
 void sigintHandler(int sig_num){
     signal(SIGINT, sigintHandler);
     fprintf(stderr,"\n Cannot be terminated using Ctrl+C \n");
     fflush(stdout);
 }
-int ownCmdHandler(char** parsed){
-    printf("first");
 
-    int NoOfOwnCmds = 8, i, switchOwnArg = 0;
-    char* ListOfOwnCmds[NoOfOwnCmds];
-    char* username;
-    printf("vfdgkjfhewl");
-    ListOfOwnCmds[0] = "exit";
-    ListOfOwnCmds[1] = "cd";
-    ListOfOwnCmds[2] = "tl";
-    ListOfOwnCmds[3] = "mf";
-    ListOfOwnCmds[4] = "rs";
-    ListOfOwnCmds[5] = "uc";
-    ListOfOwnCmds[6] = "nl";
-    ListOfOwnCmds[7] = "fw";
+int commandHandler(char** parsed){
 
+    int ncmds = 8, i, cmdNumber = 0;
+    char* cmdlists[ncmds];
+    cmdlists[0] = "exit";
+    cmdlists[1] = "cd";
+    cmdlists[2] = "tl";
+    cmdlists[3] = "mf";
+    cmdlists[4] = "rs";
+    cmdlists[5] = "uc";
+    cmdlists[6] = "nl";
+    cmdlists[7] = "fw";
 
-    for (i = 0; i < NoOfOwnCmds; i++) {
-        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
-            printf("compare");
-
-            switchOwnArg = i + 1;
+    for (i = 0; i < ncmds; i++) {
+        if (strcmp(parsed[0], cmdlists[i]) == 0) {
+            cmdNumber = i + 1;
             break;
         }
     }
-
-    switch (switchOwnArg) {
-        case 1:
-            printf("\nGoodbye\n");
-            exit(0);
-        case 2:
-            chdir(parsed[1]);
-            return 1;
-        case 3:
-            parsed[0] = "head";
-            parsed[2] = NULL;
-            return 0;
-        case 4:
-            parsed[2] = parsed[1];
-            parsed[0] = "awk";
-            parsed[1] = "max < ++c[$0] {max = c[$0]; line = $0} END {print line}";
-            return 0;
-        case 5:
-            parsed[2] = parsed[1];
-            parsed[0] = "sed";
-            parsed[1] = "s/[[:blank:]$SPACES]//g";
-            parsed[3] = NULL;
-            return 0;
-        case 6:
-            parsed[3] = parsed[1];
-            parsed[0] = "grep";
-            parsed[1] = "-v";
-            parsed[2] = "^#";
-            parsed[4] = NULL;
-            return 0;
-        case 7:
-            printf("nl");
-
-            parsed[2] = parsed[1];
-            parsed[0] = "wc";
-            parsed[1] = "-l";
-            // parsed[2] = "<";
-            parsed[3] = NULL;
-            return 0;
-        case 8:
-            parsed[2] = parsed[1];
-            parsed[0] = "awk";
-            parsed[1] = "{print $1}";
-            parsed[3] = NULL;
-            return 0;
-        default:
-            break;
+    if(cmdNumber==1){
+        printf("\nEXited Successfully\n");
+        exit(0);
     }
+    if(cmdNumber==2){
+        chdir(parsed[1]);
+        return 1;
+    }
+    if(cmdNumber==3){
+        parsed[0] = "head";
+        parsed[2] = NULL;
+        return 0;
 
+    }
+    if(cmdNumber==4){
+        parsed[2] = parsed[1];
+        parsed[0] = "awk";
+        parsed[1] = "max < ++c[$0] {max = c[$0]; line = $0} END {print line}";
+        parsed[3] = NULL;
+        return 0;
+
+    }
+    if(cmdNumber==5){
+        parsed[2] = parsed[1];
+        parsed[0] = "sed";
+        parsed[1] = "s/[[:blank:]$SPACES]//g";
+        parsed[3] = NULL;
+        return 0;
+
+    }
+    if(cmdNumber==6){
+        parsed[3] = parsed[1];
+        parsed[0] = "grep";
+        parsed[1] = "-v";
+        parsed[2] = "^#";
+        parsed[4] = NULL;
+        return 0;
+
+    }
+    if(cmdNumber==7){
+        parsed[2] = parsed[1];
+        parsed[0] = "wc";
+        parsed[1] = "-l";
+        parsed[3] = NULL;
+        return 0;
+
+    }
+    if(cmdNumber==8){
+        parsed[2] = parsed[1];
+        parsed[0] = "awk";
+        parsed[1] = "{print $1}";
+        parsed[3] = NULL;
+        return 0;
+
+    }
     return 0;
 }
+
 int receiveCommand(char* str)
 {
     char* buffer;
@@ -139,4 +138,3 @@ int main() {
     return 0;
 
 }
-
